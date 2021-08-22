6159cb28cffc5d92404cee816bd3c6fd55eda2fe
Yuzhnii <70344381+108etep@users.noreply.github.com>
2021-08-22 23:46:40 +0500

    new

---
+++ js/main.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+
+
+    $('.slider').slick({
+       arrows: true,
+       prevArrow: '<div class="slick-prev"></div>',
+       nextArrow: '<div class="slick-next"></div>',
+       autoplay: false,
+        // asNavFor: '.thumbs',
+      });
+      // $('.thumbs').slick({
+      //   arrows: false,
+      //   slidesToShow: 6,
+      //   slidesToScroll:6,
+      //   asNavFor: '.slider',
+      //   focusOnSelect: true,
+        
+  
+        
+      // });
+      
+
+
+  });
